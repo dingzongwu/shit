ade4955087c170ca4c2a4d9033effa4d976cabe0
DingZongWu <359678376@qq.com>
2013-07-20 12:45:20 +0800

    add project

---
+++ 110fuck/110fuck/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  110fuck
+//
+//  Created by Ibokan on 13-7-20.
+//  Copyright (c) 2013年 Ibokan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
